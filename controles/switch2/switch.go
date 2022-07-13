9795392847bd5ef71bb81e4badfd9a20bc636003
Eduardo Pooter Reis <edupooterreis@gmail.com>
2022-07-13 18:02:46 -0300

    Added switch block without condition; Refactored if/else using blank switch.

---
+++ controles/switch2/switch.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+
+	// Executa a primeira condição verdadeira
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Bom dia")
+	case t.Hour() < 18:
+		fmt.Println("Boa tarde")
+	default:
+		fmt.Println("Boa noite")
+	}
+}
