f847908aeeb72b328e3bb535f86a6f3c672b7eb8
Eduardo Pooter Reis <edupooterreis@gmail.com>
2022-07-13 16:54:19 -0300

    Added a if/else/if example.

---
+++ controles/ifelseif/ifelseif.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func notaParaConceito(n float64) string {
+	if n >= 9 && n <= 10 {
+		return "A"
+	} else if n >= 8 && n < 9 {
+		return "B"
+	} else if n >= 5 && n < 8 {
+		return "C"
+	} else if n >= 3 && n < 5 {
+		return "D"
+	} else {
+		return "E"
+	}
+}
+
+func main() {
+	var conceito string = notaParaConceito(1.5)
+	fmt.Println(conceito)
+}
