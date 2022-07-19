a9388b7eea755e9a4e2ea2ef36bb7aca7026487c
Eduardo Pooter Reis <edupooterreis@gmail.com>
2022-07-19 19:42:44 -0300

    Created a new package to be imported in another script.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/edupooter/curso-golang-cod3r
+
+go 1.18
---
+++ pacote/reuso/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/edupooter/curso-golang-cod3r/area"
+)
+
+func main() {
+	fmt.Println(area.CalcularAreaCirculo(6.5))
+	fmt.Println(area.CalcularAreaRetangulo(45.9, 12.5))
+	// fmt.Println(area._CalcularAreaTrianguloEquilatero(45.9, 12.5))
+}
