aea19e5b8e3674ecae19fe29ee61b4b2f9c0d87a
Eduardo Pooter Reis <edupooterreis@gmail.com>
2022-07-25 18:40:28 -0300

    Used an external package as an example.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/edupooter/curso-golang-cod3r
 
 go 1.18
+
+require github.com/edupooter/goarea v0.0.0-20220719230517-f6040c375b38 // indirect
---
+++ pacote/usandolib/usandolib.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/edupooter/goarea"
+	"github.com/edupooter/goarea/abc"
+)
+
+func main() {
+	fmt.Println(goarea.CalcularAreaCirculo(4.22))
+	fmt.Println(abc.Epa())
+}
