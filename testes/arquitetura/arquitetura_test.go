03798db2d7a116be5b063239c6e2fa8325e28717
Eduardo Pooter Reis <edupooterreis@gmail.com>
2022-09-19 19:40:40 -0300

    Worked with tests checking the architecture and parallel tests.

---
+++ testes/arquitetura/arquitetura_test.go
@@ -0,0 +1,14 @@
+package arquitetura
+
+import (
+	"runtime"
+	"testing"
+)
+
+func TestDependente(t *testing.T) {
+	t.Parallel()
+	if runtime.GOARCH == "amd64" {
+		t.Skip("Não funciona na arquitetura AMD64")
+	}
+	t.Fail()
+}
--- testes/matematica/matematica_test.go
+++ testes/matematica/matematica_test.go
@@ -5,6 +5,7 @@ import "testing"
 const erroPadrao = "Valor esperado era %v e valor resultado foi %v"
 
 func TestObterMedia(t *testing.T) {
+	t.Parallel()
 	valorEsperado := 7.13
 
 	valorResultado := Media(7.2, 9.9, 6.1, 5.3)
